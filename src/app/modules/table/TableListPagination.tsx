03ba17150ac1692ba0ade245ec2606064e3c2a63
Hadil Charafeddine <hadilnc01@gmail.com>
2023-02-03 09:54:33 -0500

    added new primary color and logo to krystalum

--- src/app/modules/table/TableListPagination.tsx
+++ src/app/modules/table/TableListPagination.tsx
@@ -5,89 +5,91 @@ import {useQueryRequest} from './QueryRequestProvider'
 import {FC} from 'react'
 
 const mappedLabel = (label: string): string => {
-  if (label === '&laquo; Previous') {
-    return 'Previous'
-  }
+    if (label === '&laquo; Previous') {
+        return 'Previous'
+    }
 
-  if (label === 'Next &raquo;') {
-    return 'Next'
-  }
+    if (label === 'Next &raquo;') {
+        return 'Next'
+    }
 
-  return label
+    return label
 }
 
 const TableListPagination = ({numbers = true}) => {
-  const pagination = useQueryResponsePagination()
+    const pagination = useQueryResponsePagination()
 
-  return (
-    <div className='row'>
-      <div className='col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'></div>
-      <div className='col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'>
-        <ul className='pagination'>
-          {pagination.links
-            ?.map((link: any) => {
-              return {...link, label: mappedLabel(link.label)}
-            })
-            .map((link: any, index: number) =>
-              !numbers ? (
-                (link.label === 'Previous' || link.label === 'Next') && (
-                  <PaginationItem key={`pagination-key-${index}`} link={link} />
-                )
-              ) : (
-                <PaginationItem key={`pagination-key-${index}`} link={link} />
-              )
-            )}
-        </ul>
-      </div>
-    </div>
-  )
+    return (
+        <div className='row'>
+            <div
+                className='col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'></div>
+            <div className='col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'>
+                <ul className='pagination'>
+                    {pagination.links
+                        ?.map((link: any) => {
+                            return {...link, label: mappedLabel(link.label)}
+                        })
+                        .map((link: any, index: number) =>
+                            !numbers ? (
+                                (link.label === 'Previous' || link.label === 'Next') && (
+                                    <PaginationItem key={`pagination-key-${index}`} link={link}/>
+                                )
+                            ) : (
+                                <PaginationItem key={`pagination-key-${index}`} link={link}/>
+                            )
+                        )}
+                </ul>
+            </div>
+        </div>
+    )
 }
 
-const PaginationItem: FC<{link: any}> = ({link}) => {
-  const pagination = useQueryResponsePagination()
-  const isLoading = useQueryResponseLoading()
-  const {updateState} = useQueryRequest()
+const PaginationItem: FC<{ link: any }> = ({link}) => {
+    const pagination = useQueryResponsePagination()
+    const isLoading = useQueryResponseLoading()
+    const {updateState} = useQueryRequest()
 
-  const updatePage = (page: number | undefined) => {
-    if (!page || isLoading || pagination.page === page) {
-      return
+    const updatePage = (page: number | undefined) => {
+        if (!page || isLoading || pagination.page === page) {
+            return
+        }
+
+        updateState({page, per_page: pagination.per_page || 10})
     }
 
-    updateState({page, per_page: pagination.per_page || 10})
-  }
-
-  const currentPage = (link: any) => {
-    if (link.url) {
-      const url = new URL(link.url)
-      let pageNumber = url.searchParams.get('page')
-      if (pageNumber) {
-        return parseInt(pageNumber)
-      }
+    const currentPage = (link: any) => {
+        if (link.url) {
+            const url = new URL(link.url)
+            let pageNumber = url.searchParams.get('page')
+            if (pageNumber) {
+                return parseInt(pageNumber)
+            }
+        }
     }
-  }
 
-  return (
-    <li
-      key={link.label}
-      className={clsx('page-item', {
-        active: link.active,
-        disabled: isLoading,
-        previous: link.label === 'Previous',
-        next: link.label === 'Next',
-      })}
-    >
-      <a
-        className={clsx('page-link', {
-          'page-text': link.label === 'Previous' || link.label === 'Next',
-          'me-5': link.label === 'Previous',
-        })}
-        onClick={() => updatePage(currentPage(link))}
-        style={{cursor: 'pointer'}}
-      >
-        {mappedLabel(link.label)}
-      </a>
-    </li>
-  )
+    return (
+        <li
+            key={link.label}
+            className={clsx('page-item', {
+                active: link.active,
+                disabled: isLoading,
+                previous: link.label === 'Previous',
+                next: link.label === 'Next',
+            })}
+        >
+            <a
+                className={clsx('page-link', {
+                    'page-text text-hover-krys': link.label === 'Previous' || link.label === 'Next',
+                    'me-5': link.label === 'Previous',
+                    'bg-krys': link.label !== 'Previous' && link.label !== 'Next'
+                })}
+                onClick={() => updatePage(currentPage(link))}
+                style={{cursor: 'pointer'}}
+            >
+                {mappedLabel(link.label)}
+            </a>
+        </li>
+    )
 }
 
 export {TableListPagination}
