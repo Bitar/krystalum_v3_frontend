22e48abd2fc7fd48cbaf07eac131f657d13c7283
Hadil Charafeddine <hadilnc01@gmail.com>
2023-03-03 10:49:33 -0500

    unified the error pages into one template and updated the style to match krystalum's esthetics

--- src/app/modules/errors/ErrorsPage.tsx
+++ src/app/modules/errors/ErrorsPage.tsx
@@ -1,9 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import {Route, Routes, Outlet} from 'react-router-dom'
-import {Error500} from './components/Error500'
-import {Error404} from './components/Error404'
-import {Error400} from './components/Error400';
-import {Error403} from './components/Error403';
+import TemplateErrorPage from './TemplateErrorPage';
 
 const ErrorsLayout = () => {
     return <Outlet/>
@@ -12,11 +9,11 @@ const ErrorsLayout = () => {
 const ErrorsPage = () => (
     <Routes>
         <Route element={<ErrorsLayout/>}>
-            <Route path='403' element={<Error403/>}/>
-            <Route path='404' element={<Error404/>}/>
-            <Route path='400' element={<Error400/>}/>
-            <Route path='500' element={<Error500/>}/>
-            <Route index element={<Error404/>}/>
+            <Route path='403' element={<TemplateErrorPage title={"Access Denied"} code={403} message={"It appears that you are attempting to access content that you are not authorized to view. If you believe you should have access to this resource, please contact the RDI team at MMPWW for assistance."}/>}/>
+            <Route path='404' element={<TemplateErrorPage code={404} title={"Resource Not Found"} message={"We're sorry, the resource you requested could not be found. Please check the URL and try again."}/>}/>
+            <Route path='400' element={<TemplateErrorPage code={400} title={"Invalid Request"} message={"We're sorry, but the request you submitted was invalid. Please check the input and try again."}/>}/>
+            <Route path='500' element={<TemplateErrorPage code={500} title={"Server Error"} message={"We're sorry, but an internal server error occurred while trying to fulfill your request. Please contact the RDI team at MMPWW for assistance."}/>}/>
+            <Route index element={<TemplateErrorPage code={404} title={"Resource Not Found"} message={"We're sorry, the resource you requested could not be found. Please check the URL and try again."}/>}/>
         </Route>
     </Routes>
 )
