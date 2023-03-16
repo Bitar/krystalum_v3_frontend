050165a8a92af672f2528880aead2a46c310300d
Mona Al Fouani <monaalfouani@Monas-MacBook-Pro.local>
2023-03-16 14:49:01 +0200

    Removed unneeded imports

--- src/app/sections/supply/publishers/pages/Create.tsx
+++ src/app/sections/supply/publishers/pages/Create.tsx
@@ -1,4 +1,4 @@
-import React, {createRef, forwardRef, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {useNavigate} from 'react-router-dom';
 import {ErrorMessage, Field, Form, Formik} from 'formik';
