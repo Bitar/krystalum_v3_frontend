013251d30d43d4fa03c8ecee1857f017c6fbb3d7
Mona Al Fouani <monaalfouani@Monas-MacBook-Pro.local>
2023-03-17 09:53:56 +0200

    Removed unused useRef in IndexFilter files

--- src/app/sections/misc/campaign-types/partials/IndexFilter.tsx
+++ src/app/sections/misc/campaign-types/partials/IndexFilter.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {ErrorMessage, Field, Form, Formik} from 'formik';
 import {Col, Collapse, Row} from 'react-bootstrap';
 
