a5386f6701caf007ed2792f57f6b920f291db714
Hadil Charafeddine <hadilnc01@gmail.com>
2023-03-24 11:16:42 -0400

    changed the search icon font size in krys table search filter

--- src/app/components/tables/KrysTableSearchFilter.tsx
+++ src/app/components/tables/KrysTableSearchFilter.tsx
@@ -42,8 +42,8 @@ const KrysTableSearchFilter: React.FC<Props> = ({slug}) => {
                         <Row>
                             <Col md={4}>
                                 <div className="d-flex align-items-center position-relative my-1">
-                                    <i className="fa-duotone fa-magnifying-glass fs-1 position-absolute ms-6"></i>
-                                    <Field className="form-control ps-14" type="text"
+                                    <i className="fa-duotone fa-magnifying-glass fs-4 position-absolute ms-6"></i>
+                                    <Field className="form-control ps-13" type="text"
                                            placeholder="Search ..." name="search"/>
                                 </div>
                             </Col>
