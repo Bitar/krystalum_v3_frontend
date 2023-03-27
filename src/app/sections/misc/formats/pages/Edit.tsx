1a76cedbefdf0dfdce68eba29f0bbf3b3fa0eaca
Mona Al Fouani <monaalfouani@Monas-MacBook-Pro.local>
2023-03-27 09:50:06 +0300

    Changed the filterData to accept array of exclude values

--- src/app/sections/misc/formats/pages/Edit.tsx
+++ src/app/sections/misc/formats/pages/Edit.tsx
@@ -77,7 +77,7 @@ const FormatEdit: React.FC = () => {
                 } else {
                     // if we were able to get the list of formats, then we fill our state with them
                     if (response.data) {
-                        setFormats(filterData(response.data, 'name', 'All Formats'));
+                        setFormats(filterData(response.data, 'name', ['All Formats']));
                     }
                 }
             });
