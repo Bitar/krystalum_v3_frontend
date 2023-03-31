a011e9b571a3f6f44300f9fd3bee645dc199b25f
Mona Al Fouani <monaalfouani@Monas-MacBook-Pro.local>
2023-03-31 16:50:52 +0300

    Removed actions from account managers for publishers

--- src/app/sections/supply/publishers/core/edit/account-managers/TableColumns.tsx
+++ src/app/sections/supply/publishers/core/edit/account-managers/TableColumns.tsx
@@ -1,14 +1,9 @@
 import {Column} from 'react-table'
 import React from 'react';
 
-import {QUERIES} from '../../../../../../../_metronic/helpers';
-
 import {CustomHeader} from '../../../../../../modules/table/columns/CustomHeader';
 import {TextCell} from '../../../../../../modules/table/columns/TextCell';
-import {Restricted} from '../../../../../../modules/auth/AuthAccessControl';
-import {ActionsCell} from '../../../../../../modules/table/columns/ActionsCell';
 import {PublisherAccountManager} from '../../../../../../models/supply/publisher/PublisherAccountManager';
-import {usePublisher} from '../../PublisherContext';
 import {formatDateToMonthDayYear} from '../../../../../../helpers/dateFormatter';
 import {BadgeCell} from '../../../../../../modules/table/columns/BadgeCell';
 
@@ -35,34 +30,7 @@ const PublisherAccountManagersColumns: ReadonlyArray<Column<PublisherAccountMana
         Cell: ({...props}) => <BadgeCell status={props.data[props.row.index].status ? 'Active' : 'Inactive'}
                                          color={props.data[props.row.index].status ? 'light-success' : 'light-danger'}
                                          align="left"/>,
-    },
-    {
-        Header: (props) => (
-            <Restricted to="manage-supply">
-                <CustomHeader tableProps={props} title="Actions" className="text-end min-w-100px"/>
-            </Restricted>
-        ),
-        id: 'actions',
-        Cell: ({...props}) => {
-            const {publisher} = usePublisher();
-
-            return (
-                props.data[props.row.index].user ?
-                    <Restricted to={'manage-supply'}>
-                        <ActionsCell
-                            id={props.data[props.row.index].id}
-                            path={`supply/publishers/${publisher?.id}/account-managers`}
-                            queryKey={QUERIES.PUBLISHER_ACCOUNT_MANAGERS_LIST}
-                            showView={false}
-                            showEdit={false}
-                            showDelete={props.data[props.row.index].status ? true : false}
-                            title="Delete Publisher Account Manager"
-                            text={`Are you sure you want to delete the publisher account manager '${props.data[props.row.index].name}'?`}
-                        />
-                    </Restricted> : <></>
-            )
-        },
-    },
+    }
 ]
 
 export {PublisherAccountManagersColumns}
