b325fba7915b3a225e37e0bd684129e8541072be
Mona Al Fouani <monaalfouani@Monas-MacBook-Pro.local>
2023-05-24 15:41:06 +0300

    Reformat code

--- src/app/sections/supply/publishers/pages/Edit.tsx
+++ src/app/sections/supply/publishers/pages/Edit.tsx
@@ -128,22 +128,24 @@ const PublisherEdit: React.FC = () => {
                         <div className="row">
                             <div className="col-lg-4 col-xl-3">
                                 <Nav variant="pills" className="flex-column settings-nav">
-                                    {settingsNav.map((settings, index) => (
-                                        <Nav.Item key={`settings-nav-${index}`} className="mb-5">
-                                            <Nav.Link className="settings-nav-item"
-                                                      eventKey={`settingsNav-${settings.key}`}>
-                                                <div className="settings-nav-icon w-25px h-25px bg-transparent">
-                                                    <i className={`${settings.icon}`}></i>
-                                                </div>
-                                                <div className="settings-nav-label">
+                                    {
+                                        settingsNav.map((settings, index) => (
+                                            <Nav.Item key={`settings-nav-${index}`} className="mb-5">
+                                                <Nav.Link className="settings-nav-item"
+                                                          eventKey={`settingsNav-${settings.key}`}>
+                                                    <div className="settings-nav-icon w-25px h-25px bg-transparent">
+                                                        <i className={`${settings.icon}`}></i>
+                                                    </div>
+                                                    <div className="settings-nav-label">
                                                 <span
                                                     className="settings-nav-title text-gray-800">{settings.title}</span>
-                                                    <span
-                                                        className="settings-nav-desc text-gray-500">{settings.description}</span>
-                                                </div>
-                                            </Nav.Link>
-                                        </Nav.Item>
-                                    ))}
+                                                        <span
+                                                            className="settings-nav-desc text-gray-500">{settings.description}</span>
+                                                    </div>
+                                                </Nav.Link>
+                                            </Nav.Item>
+                                        ))
+                                    }
                                 </Nav>
                             </div>
                             <div className="col-lg-8 col-xl-9">
