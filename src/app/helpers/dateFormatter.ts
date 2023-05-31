8d55eb5139eaecbef9bb05f078952eb5cb4617b3
Hadil Charafeddine <hadilnc01@gmail.com>
2023-05-31 09:59:31 -0400

    added the createDateFromString to the dateFormatter helper to be used to fill the date picker after getting the date from the backend

--- src/app/helpers/dateFormatter.ts
+++ src/app/helpers/dateFormatter.ts
@@ -4,3 +4,8 @@ export const formatDateToMonthDayYear = (date: Date) => {
 
     return `${fullDate.toLocaleString('default', {month: 'long'})} ${fullDate.getDate()}, ${fullDate.getFullYear()}`;
 }
+
+export const createDateFromString = (date: string) => {
+    // for the month we do -1 because new Date accepts month Index and not month
+    return new Date(parseInt(date.split('-')[0]), parseInt(date.split('-')[1]) - 1, parseInt(date.split('-')[2]));
+}
